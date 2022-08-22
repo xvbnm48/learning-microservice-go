4953339e16626a55c0f70a4471cc53fce51131b5
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-08-22 22:11:52 +0700

    handlers, helpers for error, and write json

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	// if go version is 1.18 using any, if go version is not 1.18 using interface
-	Data any `json:"data, omitempty"`
-}
+// type jsonResponse struct {
+// 	Error   bool   `json:"error"`
+// 	Message string `json:"message"`
+// 	// if go version is 1.18 using any, if go version is not 1.18 using interface
+// 	Data any `json:"data, omitempty"`
+// }
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
@@ -18,8 +17,7 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "hit the broker ",
 	}
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	// out, _ := json.MarshalIndent(payload, "", "\t")
+	_ = app.writeJSON(w, http.StatusOK, payload)
+
 }
