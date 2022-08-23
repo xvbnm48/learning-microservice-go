acd759a0c842e6710958141de5d4bd1da8f09f75
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-08-23 23:03:58 +0700

    auth service: make function models like get one, insert new user, delete and find

--- authentication-service/data/models.go
+++ authentication-service/data/models.go
@@ -5,6 +5,8 @@ import (
 	"database/sql"
 	"log"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 const dbTimeOut = time.Second * 3
@@ -106,4 +108,96 @@ func (u *User) GetByEmail(email string) (*User, error) {
 
 	return &user, nil
 
+}
+
+// GetOne: return one user by id
+func (u *User) GetOne(id string) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeOut)
+	defer cancel()
+
+	query := `SELECT id, email, firts_name, last_name, password, active, created_at, updated_at FROM users WHERE id = $1`
+
+	var user User
+	row := db.QueryRowContext(ctx, query, id)
+	err := row.Scan(
+		&user.ID,
+		&user.Email,
+		&user.FirtsName,
+		&user.LastName,
+		&user.Password,
+		&user.Active,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+// Update: updates one user in the database, using the information
+// stored in the receiver u
+func (u *User) Update() error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeOut)
+	defer cancel()
+
+	stmt := `UPDATE users set email = $1, firts_name = $2, last_name = $3, user_active = $4, updated_at = $5 WHERE id = $6`
+
+	_, err := db.ExecContext(ctx, stmt, u.Email, u.FirtsName, u.LastName, u.Active, time.Now(), u.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Delete: deletes one user from the database, by user id
+func (u *User) Delete() error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeOut)
+	defer cancel()
+
+	stmt := `DELETE FROM users WHERE id = $1`
+	_, err := db.ExecContext(ctx, stmt, u.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeleteById deletes one user from the database, by id
+func (u *User) DeleteById(id int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeOut)
+	defer cancel()
+
+	stmt := `DELETE FROM users WHERE id = $1`
+	_, err := db.ExecContext(ctx, stmt, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Insert: Inserts a new user into the database, and returns the ID of the newly inserted row
+func (u *User) Insert(user User) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeOut)
+	defer cancel()
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), 12)
+	if err != nil {
+		return 0, err
+	}
+
+	var newId int
+	stmt := `INSERT INTO users (email, firts_name, last_name, password, active, created_at, updated_at) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`
+
+	err = db.QueryRowContext(ctx, stmt, user.Email, user.FirtsName, user.LastName, hashedPassword, user.Active, time.Now(), time.Now()).Scan(&newId)
+
+	if err != nil {
+		return 0, err
+	}
+	return newId, nil
 }
--- authentication-service/go.mod
+++ authentication-service/go.mod
@@ -1,3 +1,5 @@
 module authentication
 
 go 1.19
+
+require golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8
